849311a8c4654d39003527d1573a0ab66003b290
Edward Tladi <Edward.Tladi@mixtelematics.com>
2023-04-07 22:32:42 +0200

    List to Array and Array to List Functions

--- List/main.c
+++ List/main.c
@@ -1,8 +1,25 @@
-/*
-  The ADT List
-  by Edward Tladi
-  .... date
-*/
+
+/* =============================================================================
+ |   Project   :  DATA STRUCTURES AND ALGORITHS IN C
+ |
+ |   Author    :  EDWARD TLADI
+ |   Language  :  ANSI C LANG
+ |   To Compile:  gcc main.c 
+ |
+ |   Date      :  05 /04/2023
+ |
+ +-----------------------------------------------------------------------------
+ |
+ |  Description:  Learning Link list in C 
+ |
+ |    Algorithm:  OUTLINE THE APPROACH USED BY THE PROGRAM TO SOLVE THE
+ |      PROBLEM.
+ |
+ |   Required Features Not Included: 
+ |
+ |   Known Bugs:  
+ |
+ *=========================================================================== */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +31,13 @@ typedef struct node
     int data; 
     struct node *next_node;
 } Node;
+
+typedef union
+{
+    /* data */
+    int x;
+}Student;
+
 
 /* function : initializing a list */
 void Initialize_Node(Node* node){ node = NULL; }
@@ -37,6 +61,27 @@ Node* append_to_front(int data, Node* front_node)
     return head;
 }
 
+ /*--------------------- append_node(Node*) -------------------------
+|  Function FUNCTION_NAME
+|
+|  Purpose:  
+|  
+|  Parameters:
+|  
+|  Returns:  
+*-------------------------------------------------------------------*/
+void append_node(Node* head_of_list, Node* new_node )
+{
+    if(!is_empty(new_node)  && !is_empty(head_of_list))
+    {
+        new_node->next_node = head_of_list;
+    }
+    else
+    {
+        printf("The node has not been created \n You can create a new Node by Create_Node Function\n");
+    }
+}
+
 /* Appeding a node at back of link list */
 Node* append_to_the_back(int data, Node* last_node)
 {
@@ -47,7 +92,7 @@ Node* append_to_the_back(int data, Node* last_node)
 
 int search(int value, Node* head)
 {
-   printf("Searching ...\n");
+   printf("Searching node...\n");
    while (!is_empty(head))
    {
       /* Worse case scenrio is that, the data is not in the first node */
@@ -61,10 +106,64 @@ int search(int value, Node* head)
 
    return -1;
    
+} // 
+
+/* A function to insert a node between nodes in the list */
+void insert_node(Node* first_node, int data, int position)
+{
+     printf("Inserting a node at position %d \n", position); 
+
+     Node* new_node = create_node(data); // creating a node
+
+     int loop  = 0; // init loop
+     if(!is_empty(first_node))
+     {
+        printf("The list is not EMPTY\n");
+        printf("the content %d\n", first_node->data);
+     }
+
+     while (!is_empty(first_node)) // start while loop
+     {
+        
+        loop ++; // increment the loop
+        if(loop == position && !(position<=1) )
+        {
+            // point where the prev node was pointing to, 
+            new_node->next_node = first_node->next_node; 
+           // then, redirect the prev node to point to a new node
+            first_node->next_node = new_node;
+            break; // break the loop
+        }
+        
+        // update the node
+        first_node = first_node->next_node;
+     } // end of while loop
+     
+} 
+
+// del
+void delete_node(Node* first_node, int data)
+{
+    
+    if(search(data, first_node)==1)
+    {
+        printf("check if is right node : %d\n", first_node->data);
+        while (!is_empty(first_node))
+        {
+            /* code */
+
+
+        }
+    }
+    else
+    {
+        printf("Node %d is not in the list :\n", data);
+    }
+    
 }
 
 /*  Printing the values in the linked list */
-void print_list(Node* node, char* title)
+void PrintList(Node* node, char* title)
 {
     printf("%s\n", title);
     while (!is_empty(node))
@@ -73,57 +172,61 @@ void print_list(Node* node, char* title)
         printf("%d  ", node->data);
         node = node->next_node;
     }
+
+    printf("\n");
     
 }
 
 /* A function to convert array to list */
-Node* ArrayToList(int arr[], int size, Node* first_node)
+Node* ArrayToList(int arr[], int size) 
 {
+    Node *first_node = create_node(arr[size-1]); // creating first node
+    for (int i = (size-2); i>=0; i--) 
+    {
+        Node* new_node = append_to_front(arr[i], first_node); // append a new node in front
+        first_node = new_node; // update a first node
+    }
    
-   // initialize  the list 
-   Initialize_Node(first_node);
-   
-   // The first node will be at pos 0
-   first_node = create_node(arr[0]);
+    return first_node; // return new node
+}
 
-   for (int i = 1; i <= size - 1; i++)
-   {
-     
-            /* append the data to the list */
-           Node *node = append_to_the_back(arr[i], first_node);
-       
-       
-       
-       // the new start now is the next node
-       //first_node = node;
-   }
+void ListToArray(Node* first_node, int array[])
+{
+    int size = 0;
     
-   print_list(first_node, "Single Element list");
-   return first_node;
-     
+    while (!is_empty(first_node))
+    {
+        /* code */
+        array[size] = first_node->data;
+        size++;
+        first_node = first_node->next_node;
+    }
+}
+
+void PrintArray(int arr[], int size)
+{
+    for(int i =0 ; i<size ; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+     printf("\n");
 }
 
 
-/*
-
-Initialize_Node(first_node);
-    int arr[10]  = {20,30,40,50,60};
-
-    first_node = create_node(arr[0]);
-
-    Node* node_2 = append_to_the_back(arr[1], first_node);
-    Node* node_3 = append_to_the_back(arr[2], node_2);
-    Node* node_4 = append_to_the_back(arr[3], node_3);
-    Node* node_5 = append_to_the_back(arr[4], node_4);
-
-*/
 int main()
 {
-    Node  *first_node;
-    int arr[5]  = {20,30,40,50,60};
-    first_node = ArrayToList(arr, 5, first_node);
+    Node*  first_node;
     
-    print_list(first_node, "Single Element list");
+    Initialize_Node(first_node);
+    int arr[5]  = {20,30,40,50,60};
+    int array[5] = {0}; // init array
+
+    first_node = ArrayToList(arr,5);
+    
+    
+    PrintList(first_node, "Single Element list");
+    ListToArray(first_node, array);
+    PrintArray(array,5);
     
        
     printf("\n\n");
